41ecfe705ea372ab7aec86af0f6c911d56c32a04
vakhtangaA <vakhtang.chitauri@gmail.com>
2021-07-02 21:41:08 +0400

    change localhost to heroku url

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import validator from "validator";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [longUrl, setLongUrl] = useState("");
+  const [shortUrl, setShortUrl] = useState("");
+
+  const handleLongUrl = e => {
+    setLongUrl(e.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    const validURL = validator.isURL(longUrl, {
+      require_protocol: true,
+    });
+
+    if (!validURL) {
+      alert("Please ensure this url is correct and includes http(s) protocol");
+    } else {
+      axios
+        .post("https://url-shortener-gol.herokuapp.com/", {
+          url: longUrl,
+        })
+        .then(res => {
+          setShortUrl(
+            `https://url-shortener-gol.herokuapp.com/${res.data.hash}`
+          );
+        })
+        .catch(err => console.log(err));
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Enter URL"
+            onChange={handleLongUrl}
+            value={longUrl}
+          />
+          <input type="submit" value="shorten" />
+        </form>
+        <a href={longUrl}>{shortUrl}</a>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
